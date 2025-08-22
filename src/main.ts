69e918964a433facabaeb2a424f0c2fd784affe8
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-08-22 20:40:15 +0200

    UPD - Snack 1 BONUS done

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-let unknownData: unknown = {};
+// Snack 1 + BONUS
+
+let unknownData: unknown = null;
 
 if (typeof unknownData === 'string') {
   console.log(unknownData.toUpperCase());
 } else if (typeof unknownData === 'number') {
   console.log(unknownData * 2);
 } else if (typeof unknownData === 'boolean') {
-  unknownData ? console.log('Si') : console.log('No');
+  console.log(unknownData ? 'Si' : 'No');
+} else if (unknownData === null) {
+  console.log('Il dato è vuoto');
+} else if (unknownData instanceof Array) {
+  console.log(unknownData.length);
+} else if (unknownData instanceof Promise) {
+  unknownData
+    .then(value => console.log(value))
+    .catch(e => console.log(e))
 } else {
   console.log('Tipo non supportato');
 }
