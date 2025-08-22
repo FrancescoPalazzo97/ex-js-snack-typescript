11890bcbb62814a3a27dd3f705c3c4b81f4b89f9
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-08-22 20:47:38 +0200

    UPD - Snack 2 done

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,39 @@
 // Snack 1 + BONUS
 
-let unknownData: unknown = null;
+// let unknownData: unknown = null;
 
-if (typeof unknownData === 'string') {
-  console.log(unknownData.toUpperCase());
-} else if (typeof unknownData === 'number') {
-  console.log(unknownData * 2);
-} else if (typeof unknownData === 'boolean') {
-  console.log(unknownData ? 'Si' : 'No');
-} else if (unknownData === null) {
-  console.log('Il dato è vuoto');
-} else if (unknownData instanceof Array) {
-  console.log(unknownData.length);
-} else if (unknownData instanceof Promise) {
-  unknownData
-    .then(value => console.log(value))
-    .catch(e => console.log(e))
-} else {
-  console.log('Tipo non supportato');
+// if (typeof unknownData === 'string') {
+//   console.log(unknownData.toUpperCase());
+// } else if (typeof unknownData === 'number') {
+//   console.log(unknownData * 2);
+// } else if (typeof unknownData === 'boolean') {
+//   console.log(unknownData ? 'Si' : 'No');
+// } else if (unknownData === null) {
+//   console.log('Il dato è vuoto');
+// } else if (unknownData instanceof Array) {
+//   console.log(unknownData.length);
+// } else if (unknownData instanceof Promise) {
+//   unknownData
+//     .then(value => console.log(value))
+//     .catch(e => console.log(e))
+// } else {
+//   console.log('Tipo non supportato');
+// }
+
+// Snack 2 + BONUS
+
+type Dipendente = {
+  nome: string,
+  cognome: string,
+  annoNascita: number,
+  sesso: 'm' | 'f',
+  anniDiServizio: number[]
 }
+
+// let dipendente: Dipendente = {
+//   nome: 'Francesco',
+//   cognome: 'Palazzo',
+//   annoNascita: 1997,
+//   sesso: 'm',
+//   anniDiServizio: [2023, 2024, 2025]
+// }
